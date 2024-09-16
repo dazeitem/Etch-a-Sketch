65c6aa6ce0136564594107a66f1980fe4d4bd8c5
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-09-16 12:33:30 +0200

    feat: add class name generator
    - random string generator added to add as class specifiers

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ let pixels = []
 // Import Alphabet for generating random class names
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+// Import sketch body
+const sketchBody = document.querySelector(".drawbody");
+
+// Generate random className
+let generateName = () => {
+    let className = '';
+    for (i = 0; i < 8; i++) {
+        className += alphabet[Math.floor(Math.random() * 26)];
+    };
+    return className
+};
