19e84c3a568ed0146939100bb8133a2b316e75a4
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-09-16 14:54:59 +0200

    minor change: convention adherence improvement
    - semicolons added as according to convention
    - unnecessary spaces removed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Initialize lists
-let rowDiv = []
-let pixels = []
+let rowDiv = [];
+let pixels = [];
 
 // Import Alphabet for generating random class names
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
@@ -11,8 +11,8 @@ const sketchBody = document.querySelector(".drawbody");
 // Generate random className
 let generateName = () => {
     let className = '';
-    for (i = 0; i < 8; i++) {
+    for (i=0; i<8; i++) {
         className += alphabet[Math.floor(Math.random() * 26)];
     };
-    return className
+    return className;
 };
